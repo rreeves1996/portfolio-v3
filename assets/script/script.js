a54b91af22f17ee262deeb445bd18793036612b1
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-13 19:46:18 -0700

    Refined layout, buttons work

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,28 @@
-const buttons = [
-    aboutButton = document.getElementById("about"),
-    portfolioButton = document.getElementById("portfolio"),
-    contactButton = document.getElementById("contact")];
+var carousel = document.querySelector('.carousel');
+const navBarButtons = [
+    aboutButtonNav = document.getElementById("about-nav"),
+    portfolioButtonNav = document.getElementById("portfolio-nav"),
+    contactButtonNav = document.getElementById("contact-nav")];
+const homeNavButtons = [
+    aboutButtonHome = document.getElementById("about-home"),
+    portfolioButtonHome = document.getElementById("portfolio-home"),
+    contactButtonHome = document.getElementById("contact-home")];
 const sections = [
     about = document.getElementById("about"),
     portfolio = document.getElementById("portfolio"),
     contact = document.getElementById("contact")];
 
 for (let i = 0; i < sections.length; i++) {
-    sideButton[i].addEventListener("click", function() {
+    navBarButtons[i].addEventListener("click", function() {
+        sections[i].scrollIntoView();
+    });
+    homeNavButtons[i].addEventListener("click", function() {
         sections[i].scrollIntoView();
     });
 }
 
-var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
+
+var flkty = new Flickity(carousel, {
   // options
   cellAlign: 'left',
   wrapAround: true,
